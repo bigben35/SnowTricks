a95d2e947e903f144aa60f63e3a4dabbaaa6f762
Charles <charles@charlesen.fr>
2023-02-02 19:20:24 +0100

    Fix bug fixture

--- src/DataFixtures/TrickFixtures.php
+++ src/DataFixtures/TrickFixtures.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 
 namespace App\DataFixtures;
 
@@ -20,22 +20,21 @@ class TrickFixtures extends Fixture
         // use the factory to create a Faker\Generatot instance
         $faker = Faker\Factory::create('fr_FR');
 
-        dd($this->userRepository->findOneBy(['email' => 'admin@admin.fr']));
-        for($trick = 1; $trick <=10; $trick++){
+        for ($trick = 1; $trick <= 10; $trick++) {
             $tricks = new Trick();
             $tricks->setName($faker->text(5));
             // $tricks->setAuthor($faker->text(5));
             // $user = $this->getReference(1, 6);
-            // $tricks->setAuthor($faker->name());
+            $tricks->setAuthor($faker->name());
             $tricks->setDescription($faker->text());
             $tricks->setSlug($this->slugger->slug($tricks->getName())->lower());
             // $tricks->setCategory($category);
             //on va chercher une réf de catégorie
-            $category = $this->getReference(''. rand(1, 5));
+            $category = $this->getReference('' . rand(1, 5));
             $tricks->addCategory($category);
             $tricks->setIllustration($faker->text(5));
             $tricks->setVideo($faker->text(5));
-            // $userId = $this->getReference(''. rand());
+
             $tricks->setUser($this->userRepository->findOneBy(['email' => 'admin@admin.fr']));
             $manager->persist($tricks);
         }
--- src/DataFixtures/UserFixtures.php
+++ src/DataFixtures/UserFixtures.php
@@ -5,6 +5,7 @@ namespace App\DataFixtures;
 use Faker;
 use Faker\Factory;
 use App\Entity\User;
+use App\Repository\UserRepository;
 use Doctrine\Persistence\ObjectManager;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Symfony\Component\String\Slugger\SluggerInterface;
@@ -12,34 +13,43 @@ use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 class UserFixtures extends Fixture
 {
-    public function __construct(private UserPasswordHasherInterface $passwordEncoder, private SluggerInterface $slugger)
-    {
-        
+    public function __construct(
+        private UserPasswordHasherInterface $passwordEncoder,
+        private SluggerInterface $slugger,
+        private UserRepository $userRepository
+    ) {
     }
     private $counter = 1; //créer un compteur pour pouvoir utiliser user_id
     public function load(ObjectManager $manager): void
     {
-        $admin = new User();
-        $admin->setEmail('admin@admin.fr');
-        $admin->setUsername('admin');
-        $admin->setPassword($this->passwordEncoder->hashPassword($admin, 'admin'));
-        $admin->setRoles(['ROLE_ADMIN']);
+        // On ne créé l'utilisateur que si n'existe pas déjà
+        if (!$this->userRepository->findOneByEmail('admin@admin.fr')) {
+            $admin = new User();
 
-        $manager->persist($admin);
+            $admin->setEmail('admin@admin.fr');
+            $admin->setUsername('admin');
+            $admin->setPassword($this->passwordEncoder->hashPassword($admin, 'admin'));
+            $admin->setRoles(['ROLE_ADMIN']);
+            $manager->persist($admin);
+        }
+
 
         $faker = Factory::create('fr_FR');
 
-        for($usr = 1; $usr <= 5; $usr++){
-            $user = new User();
-            $user->setEmail($faker->email);
-            $user->setUsername($faker->userName);
-            $user->setPassword($this->passwordEncoder->hashPassword($user, 'user'));
-            $user->setRoles(['ROLE_USER']);
+        for ($usr = 1; $usr <= 5; $usr++) {
+            // On ne créé l'utilisateur que si n'existe pas déjà
+            $userEmail = $faker->email;
+            if (!$this->userRepository->findOneByEmail($userEmail)) {
+                $user = new User();
+                $user->setEmail($userEmail);
+                $user->setUsername($faker->userName);
+                $user->setPassword($this->passwordEncoder->hashPassword($user, 'user'));
+                $user->setRoles(['ROLE_USER']);
 
-            $manager->persist($user);
-            // $this->addReference('user-'.$this->counter, $user);
-            // $this->counter++; 
-
+                $manager->persist($user);
+                // $this->addReference('user-'.$this->counter, $user);
+                // $this->counter++; 
+            }
         }
 
         $manager->flush();
@@ -52,7 +62,7 @@ class UserFixtures extends Fixture
         $username->setUsername($username);
         $manager->persist($username);
 
-        $this->addReference(''.$this->counter, $username);
+        $this->addReference('' . $this->counter, $username);
         $this->counter++;               //stock des ref que je vais rechercher sur TrickFixtures
 
         return $username;
