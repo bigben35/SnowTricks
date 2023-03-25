da4b38ee0407937d39a3d0928bcce796431c9c7d
Josselin Crenn <josselincrenn@gmail.com>
2023-03-26 00:36:29 +0100

    add avatar registration user

--- src/Form/UserType.php
+++ src/Form/UserType.php
@@ -5,7 +5,10 @@ namespace App\Form;
 use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Validator\Constraints\All;
+use Symfony\Component\Validator\Constraints\File;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
 
@@ -30,17 +33,36 @@ class UserType extends AbstractType
                 ],
                 'label' => "Email"
             ])
-            ->add('avatar', TextType::class, [
+            ->add('avatar', FileType::class, [
                 'attr' => [
                     'class' => 'form-control',
                     'placeholder' => "Avatar"
                 ],
-                'label' => "Avatar",
-                'required' => false
-            ])
-            ->add('is_verified')
-            // ->add('resetToken')
-        ;
+                'label' => "Avatar (jpg, jpeg, png)",
+                'required' => false,
+                'mapped' => false,
+                'multiple' => false,
+                'constraints' => [
+                    new All([
+                        'constraints' => [
+                            new File([
+                                'maxSize' => '1024k',
+                                'mimeTypes' => [
+                                    'image/jpeg',
+                                    'image/png',
+                                    'image/jpg',
+                                ],
+                                'mimeTypesMessage' => 'Please upload a valid image type',
+                            ])
+                        ]
+                    ])
+                ],
+                ])
+                ->add('is_verified')
+                // ->add('resetToken')
+                ;
+                $avatarType = $builder->get('avatar')->getType()->getInnerType();
+                var_dump($avatarType);
     }
 
     public function configureOptions(OptionsResolver $resolver): void
