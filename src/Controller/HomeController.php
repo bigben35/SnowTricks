a52a8accf794fe6548b19fdfc646b6514d422ec2
Josselin Crenn <josselincrenn@gmail.com>
2023-04-10 22:05:42 +0200

    crud videos + css

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -15,18 +15,7 @@ class HomeController extends AbstractController
     #[Route('/', name: 'app_home')]
     public function index(UserRepository $userRepository, TrickRepository $trickRepository): Response
     {
-        // $userRepository->findOneByEmail('admin@admin.fr');
-        // dd($userRepository->findOneByEmail('admin@admin.fr'));
-        // On récupère les 3 derniers tricks créé
         $tricks = $trickRepository->findBy([], array('created_at' => 'DESC'));
-        // $trick = $trickRepository->findBy($id);    
-        // $illustrations = $trick->getIllustrations();
-
-        // $illustration = null;
-        // if($illustrations && count($illustrations) > 0){
-        //     $illustration = $illustrations[0];
-        // }
-        // $illustrations = $tricks->getIllustrations();
 
         return $this->render('home/index.html.twig', ['tricks' => $tricks]);
     }
@@ -37,7 +26,6 @@ class HomeController extends AbstractController
     {
         $offset = $request->query->getInt('offset', 0);
         $tricks = $trickRepository->findBy([], array('created_at' => 'DESC'), 8, $offset);
-        // return $this->render('home/index.html.twig', ['tricks' => $tricks]);
          // Renvoie les données sous forme de JSON
          $response = new JsonResponse();
          $response->setData(['tricks' => $tricks]);
