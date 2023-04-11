8f5c67c7aeef8beff979d934bb9d3ebe950cf443
Charles <charles@charlesen.fr>
2023-04-11 16:58:56 +0200

    Ajout d'illustration dans les fixtures

--- src/DataFixtures/TrickFixtures.php
+++ src/DataFixtures/TrickFixtures.php
@@ -35,14 +35,30 @@ class TrickFixtures extends Fixture implements DependentFixtureInterface
         // use the factory to create a Faker\Generatot instance
         $faker = Faker\Factory::create('fr_FR');
 
-        for ($index = 1; $index <= 10; $index++) {
+        $trickList = array(
+            'Backside 180',
+            'Backside 360',
+            'Backside Caballerial',
+            'Backside Half Cab',
+            'Fakie Ollie',
+            'Frontside 180',
+            'Frontside 360',
+            'Frontside Caballerial',
+            'Frontside Half Cab',
+            'Kickturn'
+        );
+
+        for ($index = 0; $index < 10; $index++) {
             $trick = new Trick();
-            $trick->setName($faker->text(5) . ' ' . $faker->lexify());
+
+            $trickName = $trickList[$index];
+            $trick->setName($trickName);
+            // $trick->setName($faker->text(5) . ' ' . $faker->lexify());
             // trick->setAuthor($faker->text(5));
             // $user = $this->getReference(1, 6);
             // trick->setAuthor($faker->name());
             $trick->setDescription($faker->text());
-            $trick->setSlug($this->slugger->slug($trick->getName())->lower());
+            $trick->setSlug($this->slugger->slug($trickName)->lower());
 
             // Association d'un utilisateur
             $user = $this->userRepository->findOneByEmail('admin@admin.fr');
@@ -57,6 +73,12 @@ class TrickFixtures extends Fixture implements DependentFixtureInterface
             $video2 = (new Video())->setMediaLink('https://www.youtube.com/watch?v=AMH992l_nRg');
             $trick->addVideo($video1);
             $trick->addVideo($video2);
+
+
+            // Ajout d'illustrations
+            $illustration = new Illustration();
+            $illustration->setFile('trick-default/trick-example-1.jpg');
+            $trick->addIllustration($illustration);
 
             // Ajout d'un commentaire
             $comment = new CommentTrick();
